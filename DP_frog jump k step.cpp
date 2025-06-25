fc1e212c0c9a9748ad51c825ea243268ad56a708
SwatiMishra01 <swati01mishra01@gmail.com>
2025-06-25 21:33:24 +0530

    DP

--- DP_frog jump k step.cpp
+++ DP_frog jump k step.cpp
@@ -77,3 +77,30 @@ for(int i=1; i<n; i++){
     return dp[n-1];
 }
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+  ----------------------------------------
+           SPACE OPTIMIZATION
+  -----------------------------------------
+
+  //NOTE:✅  This works only when k = 2, since you just need last 2 values.
+
+int minimizeCost(int n, int k, vector<int> &height) {
+    if (n == 1) return 0;
+
+    int prev = 0;  // dp[i-1]
+    int prev2 = 0; // dp[i-2]
+    int curr = 0;
+
+    for (int i = 1; i < n; i++) {
+        int jumpOne = prev + abs(height[i] - height[i - 1]);
+        int jumpTwo = INT_MAX;
+        if (i > 1)
+            jumpTwo = prev2 + abs(height[i] - height[i - 2]);
+
+        curr = min(jumpOne, jumpTwo);
+        prev2 = prev;
+        prev = curr;
+    }
+
+    return curr;
+}
