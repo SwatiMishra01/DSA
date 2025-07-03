a3a5d9b7f54bc145ecfd76de4f78b59c67a0d115
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-03 12:54:09 +0530

    Merge sorted array with no extra space

---
+++ arrays/2 pointers/Merge sorted array with no extra space.cpp
@@ -0,0 +1,73 @@
+---------------------------
+  BRUTE FORCE
+  ------------------------
+void merge(long long arr1[], long long arr2[], int n, int m) {
+
+    //Declare a 3rd array and 2 pointers:
+    long long arr3[n + m];
+    int left = 0;
+    int right = 0;
+
+    int index = 0;
+
+    //Insert the elements from the 2 arrays
+    // into the 3rd array using left and right
+    // pointers:
+
+    while (left < n && right < m) {
+        if (arr1[left] <= arr2[right]) {
+            arr3[index] = arr1[left];
+            left++, index++;
+        }
+        else {
+            arr3[index] = arr2[right];
+            right++, index++;
+        }
+    }
+
+    // If right pointer reaches the end:
+    while (left < n) {
+        arr3[index++] = arr1[left++];
+    }
+
+    // If left pointer reaches the end:
+    while (right < m) {
+        arr3[index++] = arr2[right++];
+    }
+
+    // Fill back the elements from arr3[]
+    // to arr1[] and arr2[]:
+    for (int i = 0; i < n + m; i++) {
+        if (i < n) arr1[i] = arr3[i];
+        else arr2[i - n] = arr3[i];
+    }
+}
+T.C=O(n+m) + O(n+m)
+S.C=O(n+m) 
+
+
+--------------------------------------
+           OPTIMAL 
+--------------------------------------
+  class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;     // pointer for nums1
+        int j = n - 1;     // pointer for nums2
+        int k = m + n - 1; // pointer for placement in nums1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // Copy remaining elements of nums2 if any:
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
+
