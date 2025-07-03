27409520cb8f202673eca541a60898d66e5b5332
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-03 11:06:46 +0530

    rotate image by 90 degree

---
+++ arrays/2D/rotate image by 90 degree.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/rotate-image/
+LEETCODE-48
+---------------------------------------------
+  TRANSPOSE->REVERSE EVERY ROW)
+  -------------------------------------------
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+    //transposing the matrix
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
+    }
+};
+T.C=O(N^2)
+S.C=O(1)
