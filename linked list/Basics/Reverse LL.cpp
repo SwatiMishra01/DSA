7ac225485f236ed9ae3d31f76b0d822e6d544b73
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-13 12:30:50 +0530

    Reverse LL

---
+++ linked list/Basics/Reverse LL.cpp
@@ -0,0 +1,57 @@
+https://leetcode.com/problems/reverse-linked-list/
+LEETCODE- 206
+----------------------------------------------------
+             ITERATIVE SOLUTION
+  --------------------------------------------------
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==NULL || head->next ==NULL)
+        return head;
+
+        ListNode*prev=NULL;
+        ListNode*curr=head;
+        ListNode*forward=NULL;
+        while(curr!=NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+};
+T.C=0(N)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  -----------------------------------------------
+                RECURSIVE SOLUTION
+  -------------------------------------------------
+  Node* newHead = reverseLinkedList(head->next);
+    
+    // Save a reference to the node following
+    // the current 'head' node.
+    Node* front = head->next;
+    
+    // Make the 'front' node point to the current
+    // 'head' node in the reversed order.
+    front->next = head;
+    
+    // Break the link from the current 'head' node
+    // to the 'front' node to avoid cycles.
+    head->next = NULL;
+    
+    // Return the 'newHead,' which is the new
+    // head of the reversed linked list.
+    return newHead;
