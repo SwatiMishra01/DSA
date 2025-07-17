4a154abb1db1f0a3ff69107eb7682210d02ecd9e
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-17 09:23:38 +0530

    Sum of subarray ranges

---
+++ stacks/Sum of subarray ranges.cpp
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/sum-of-subarray-ranges/description/
+LEETCODE-2104
+
+---------------------------------
+   APPROACH 1-> BRUTE FORCE
+---------------------------------
+  class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long sum=0;
+        for(int i=0; i<nums.size(); i++){
+            int largest=nums[i];
+            int smallest=nums[i];
+                   for(int j=i+1; j<nums.size(); j++){
+                    largest=max(largest,nums[j]);
+                    smallest=min(smallest,nums[j]);
+
+                    sum+=(largest-smallest);
+                   }
+        }
+        return sum;
+    }
+};
+T.C=O(N^2)
+S.C=O(1)
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+  --------------------------------------
+   APPROACH 2-> 
+----------------------------------------
+  
+  
+  
