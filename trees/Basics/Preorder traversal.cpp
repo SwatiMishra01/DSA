ed056ed3d2f2bdb95d0d6fe25405856fd836e3b0
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-25 08:55:43 +0530

    Preorder traversal

--- trees/Basics/Preorder traversal.cpp
+++ trees/Basics/Preorder traversal.cpp
@@ -28,3 +28,60 @@ void preorder(TreeNode*root,vector<int>&ans){
         return ans;
     }
 };
+
+-----------------------------------------
+    ITERATIVE->USING STACK
+----------------------------------------
+    class Solution {
+public:
+    // Function to perform preorder traversal
+    // of a binary tree iteratively
+    vector<int> preorderTraversal(TreeNode* root) {
+        // Initialize vector to store
+        // the preorder traversal result
+        vector<int> preorder;
+        
+        // If the root is null, return
+        // an empty traversal result
+        if(root == nullptr) {
+            return preorder;
+        }
+        
+        // Create a stack to store
+        // nodes during traversal
+        stack<TreeNode*> st;
+        // Push the root node
+        // onto the stack
+        st.push(root);
+        
+        // Perform iterative preorder traversal
+        while(!st.empty()) {
+            // Get the current node
+            // from the top of the stack
+            root = st.top();
+            // Remove the node
+            // from the stack
+            st.pop();
+            
+            // Add the node's value to
+            // the preorder traversal result
+            preorder.push_back(root->val);
+            
+            // Push the right child
+            // onto the stack if exists
+            if(root->right != nullptr) {
+                st.push(root->right);
+            }
+            
+            // Push the left child onto
+            // the stack if exists
+            if(root->left != nullptr) {
+                st.push(root->left);
+            }
+        }
+        
+        // Return the preorder
+        // traversal result
+        return preorder;
+    }
+};
