5d7670614b6eb069a7a69f18d3a0332ca2316e7e
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-25 09:09:25 +0530

    Inorder traversal

--- trees/Basics/Inorder traversal.cpp
+++ trees/Basics/Inorder traversal.cpp
@@ -31,35 +31,38 @@ void inorder(TreeNode* root,vector<int>&ans){
 };
 
 --------------------------------------
-     ITERATIVE-> USING ARRAY
+     ITERATIVE-> USING STACK
 --------------------------------------
-     // Function to perform inorder traversal
-// of the tree and store values in 'arr'
-void inorder(Node* root, vector<int> &arr){
-    // If the current node is NULL
-    // (base case for recursion), return
-    if(root == nullptr){
-        return;
+     /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        vector<int>inorder;
+        TreeNode*node=root;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty()==true) break;
+                node=st.top();
+                inorder.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return inorder;
     }
-    // Recursively traverse the left subtree
-    inorder(root->left, arr);
-    // Push the current node's
-    // value into the vector
-    arr.push_back(root->data);
-    // Recursively traverse 
-    // the right subtree
-    inorder(root->right, arr);
-}
-
-// Function to initiate inorder traversal
-// and return the resulting vector
-vector<int> inOrder(Node* root){
-    // Create an empty vector to
-    // store inorder traversal values
-    vector<int> arr;
-    // Call the inorder traversal function
-    inorder(root, arr);
-    // Return the resulting vector
-    // containing inorder traversal values
-    return arr;
-}
+};
+   
