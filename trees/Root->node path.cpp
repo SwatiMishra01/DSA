6c7cf0ca862d7222a5820b11257d1435f73a0649
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-27 10:11:55 +0530

    Root->node path

--- trees/Root->node path.cpp
+++ trees/Root->node path.cpp
@@ -28,3 +28,35 @@ vector<int> Solution::solve(TreeNode* A, int B) {
 }
 T.C=O(N)
 S.C=O(N)
+
+ ---------------------------------------------
+ FOLLOW UP QUES-> ROOT TO ALL LEAF NODES PATH
+ ---------------------------------------------
+ https://leetcode.com/problems/binary-tree-paths/description/
+
+ 
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void findPath(TreeNode* node, vector<string>& ans, string temp) {
+        temp += to_string(node->val);  // Add the current node value to the path
+        if (node->left) findPath(node->left, ans, temp + "->");  // Traverse left
+        if (node->right) findPath(node->right, ans, temp + "->"); // Traverse right
+        if (!node->left && !node->right) ans.push_back(temp);  // Add path if leaf node
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+          vector<string> ans;
+        if (root) findPath(root, ans, "");  // Start traversal from the root
+        return ans;
+    }
+};
