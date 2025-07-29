69dbb634fc7cee779b2849c22048b7a8aed93eb1
SwatiMishra01 <swati01mishra01@gmail.com>
2025-07-29 16:26:11 +0530

    Inorder traversal-MORRIS

---
+++ trees/Basics/Inorder traversal-MORRIS.cpp
@@ -0,0 +1,69 @@
+https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+LEETCODE-94
+
+  /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // Vector to store the
+        // inorder traversal result
+        vector<int> inorder;
+        // Pointer to the current node,
+        // starting from the root
+        TreeNode* cur = root;
+        
+        // Loop until the current
+        // node is not NULL
+        while (cur != NULL) {
+            // If the current node's
+            // left child is NULL
+            if (cur->left == NULL) {
+                // Add the value of the current
+                // node to the inorder vector
+                inorder.push_back(cur->val);
+                // Move to the right child
+                cur = cur->right;
+            } else {
+                // If the left child is not NULL,
+                // find the predecessor (rightmost node
+                // in the left subtree)
+                TreeNode* prev = cur->left;
+                while (prev->right && prev->right != cur) {
+                    prev = prev->right;
+                }
+                
+                // If the predecessor's right child
+                // is NULL, establish a temporary link
+                // and move to the left child
+                if (prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                } else {
+                    // If the predecessor's right child
+                    // is already linked, remove the link,
+                    // add current node to inorder vector,
+                    // and move to the right child
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        
+        // Return the inorder
+        // traversal result
+        return inorder;
+    }
+};
+T.C=O(N)
+S.C=O(1)
