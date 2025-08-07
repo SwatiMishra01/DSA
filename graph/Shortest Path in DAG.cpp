2f2990eb1d2213cee7bddb306b01fd0127278f8a
SwatiMishra01 <swati01mishra01@gmail.com>
2025-08-07 09:19:12 +0530

    Shortest Path in DAG

--- graph/Shortest Path in DAG.cpp
+++ graph/Shortest Path in DAG.cpp
@@ -1,70 +1,63 @@
+https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
 -----------------------------
   USING TOPOSORT
 -----------------------------
 class Solution {
-  private:
-    void topoSort(int node, vector < pair < int, int >> adj[],
-      int vis[], stack < int > & st) {
-      //This is the function to implement Topological sort. 
-      vis[node] = 1;
-      for (auto it: adj[node]) {
-        int v = it.first;
-        if (!vis[v]) {
-          topoSort(v, adj, vis, st);
+private:
+    void dfs(int node, vector<int>& vis, stack<int>& st, vector<pair<int,int>> adj[]) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            int v = it.first;
+            if (!vis[v]) {
+                dfs(v, vis, st, adj);
+            }
         }
-      }
-      st.push(node);
+        st.push(node);
     }
-  public:
-    vector < int > shortestPath(int N, int M, vector < vector < int >> & edges) {
 
-      //We create a graph first in the form of an adjacency list.
-      vector < pair < int, int >> adj[N];
-      for (int i = 0; i < M; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        int wt = edges[i][2];
-        adj[u].push_back({v, wt}); 
-      }
-      // A visited array is created with initially 
-      // all the nodes marked as unvisited (0).
-      int vis[N] = {
-        0
-      };
-      //Now, we perform topo sort using DFS technique 
-      //and store the result in the stack st.
-      stack < int > st;
-      for (int i = 0; i < N; i++) {
-        if (!vis[i]) {
-          topoSort(i, adj, vis, st);
+public:
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        // adjacency list
+        vector<pair<int,int>> adj[V];
+        for(int i = 0; i < E; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            adj[u].push_back({v, wt});
         }
-      }
-      //Further, we declare a vector ‘dist’ in which we update the value of the nodes’
-      //distance from the source vertex after relaxation of a particular node.
 
-      vector < int > dist(N);
-      for (int i = 0; i < N; i++) {
-        dist[i] = 1e9;
-      }
-
-      dist[0] = 0;
-      while (!st.empty()) {
-        int node = st.top();
-        st.pop();
-
-        for (auto it: adj[node]) {
-          int v = it.first;
-          int wt = it.second;
-
-          if (dist[node] + wt < dist[v]) {
-            dist[v] = wt + dist[node];
-          }
+        // topological sort
+        vector<int> vis(V, 0);
+        stack<int> st;
+        for(int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, vis, st, adj);
+            }
         }
-      }
 
-      for (int i = 0; i < N; i++) {
-        if (dist[i] == 1e9) dist[i] = -1;
-      }
-      return dist;
+        // distance calculation
+        vector<int> dist(V, INT_MAX);
+        dist[0] = 0;
+        while(!st.empty()) {
+            int node = st.top();
+            st.pop();
+
+            if (dist[node] != INT_MAX) {
+                for (auto it : adj[node]) {
+                    int v = it.first;
+                    int wt = it.second;
+                    if (dist[node] + wt < dist[v]) {
+                        dist[v] = dist[node] + wt;
+                    }
+                }
+            }
+        }
+
+        // unreachable nodes
+        for(int i = 0; i < V; i++) {
+            if (dist[i] == INT_MAX) dist[i] = -1;
+        }
+
+        return dist;
     }
 };
