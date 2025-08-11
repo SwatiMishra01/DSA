497ad7692589ddaa85ac2e5c8c9aaf98ffa7c083
SwatiMishra01 <swati01mishra01@gmail.com>
2025-08-11 08:13:38 +0530

    LIS

---
+++ dp/LIS/LIS.cpp
@@ -0,0 +1,92 @@
+-------------------------------
+      RECURSION-TLE
+-----------------------------
+  class Solution {
+    private:
+    int f(int indx, int prev_indx,vector<int>& nums){
+        int n=nums.size();
+        //base case
+        if(indx==n) return 0;
+        int len=0;
+
+        //not take
+        int len1=0+f(indx+1,prev_indx,nums);
+        //take
+        int len2 = INT_MIN;
+        if(prev_indx==-1 || nums[indx]>nums[prev_indx]){
+            len2=1+f(indx+1,indx,nums);
+        }
+        return len=max(len1,len2);
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        return f(0,-1,nums);
+    }
+};
+T.C=O(2^N)
+S.C=O(N)
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  ---------------------
+       MEMOIZATION
+  ---------------------
+  class Solution {
+    private:
+    int f(int indx, int prev_indx,vector<int>& nums,vector<vector<int>>&dp){
+        int n=nums.size();
+        //base case
+        if(indx==n) return 0;
+         
+         //memoization
+         if(dp[indx][prev_indx+1]!=-1) return dp[indx][prev_indx+1];
+
+        //not take
+        int len1=0+f(indx+1,prev_indx,nums,dp);
+        //take
+        int len2 = INT_MIN;
+        if(prev_indx==-1 || nums[indx]>nums[prev_indx]){
+            len2=1+f(indx+1,indx,nums,dp);
+        }
+        return dp[indx][prev_indx+1]=max(len1,len2);
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return f(0,-1,nums,dp);
+    }
+};
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  -----------------------------
+         TABULATION
+  ------------------------------
+  class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        // dp[indx][prev_indx+1] = LIS length starting at indx with prev_indx
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // Traverse from back to front
+        for (int indx = n - 1; indx >= 0; indx--) {
+            for (int prev_indx = indx - 1; prev_indx >= -1; prev_indx--) {
+
+                // Not take
+                int len1 = dp[indx + 1][prev_indx + 1];
+
+                // Take
+                int len2 = 0;
+                if (prev_indx == -1 || nums[indx] > nums[prev_indx]) {
+                    len2 = 1 + dp[indx + 1][indx + 1];
+                }
+
+                dp[indx][prev_indx + 1] = max(len1, len2);
+            }
+        }
+
+        return dp[0][0]; // Starting from index 0 with prev_indx = -1
+    }
+};
+
+  
